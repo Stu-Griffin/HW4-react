a9e7b0626fbe794745c3b5400094236f13b5bf43
Max <maksimavramenko33@gmail.com>
2020-09-19 19:15:38 +0300

    Updates

---
+++ precache-manifest.2fde6422ac6a1c41b402bd5f400b28de.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af6755670c472d4d37d7f685079c19b",
+    "url": "/HW4-react/index.html"
+  },
+  {
+    "revision": "d75332403ad3144f577f",
+    "url": "/HW4-react/static/css/main.b2a27da9.chunk.css"
+  },
+  {
+    "revision": "4437ae9e3bc3213f9502",
+    "url": "/HW4-react/static/js/2.c6dd0a2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HW4-react/static/js/2.c6dd0a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75332403ad3144f577f",
+    "url": "/HW4-react/static/js/main.89c5db54.chunk.js"
+  },
+  {
+    "revision": "f65c614609f884d565cc",
+    "url": "/HW4-react/static/js/runtime-main.85aa77f6.js"
+  },
+  {
+    "revision": "0ec68f32e56d9b9837b8135c646f32e5",
+    "url": "/HW4-react/static/media/comment.0ec68f32.svg"
+  },
+  {
+    "revision": "2da83f119826efd1da0d332e4c363f5c",
+    "url": "/HW4-react/static/media/gender-anonim.2da83f11.svg"
+  },
+  {
+    "revision": "037e22c95230161ce14cf0fe8872b9b4",
+    "url": "/HW4-react/static/media/gender-man.037e22c9.svg"
+  },
+  {
+    "revision": "08d6db10a90680bbd77fb9bb826311b1",
+    "url": "/HW4-react/static/media/gender-woman.08d6db10.svg"
+  },
+  {
+    "revision": "70ebbcd163470cdcdd46617ad9e91610",
+    "url": "/HW4-react/static/media/likes.70ebbcd1.svg"
+  },
+  {
+    "revision": "b1f075e563c5b13b0823a137d80545a2",
+    "url": "/HW4-react/static/media/main-page.b1f075e5.jpg"
+  },
+  {
+    "revision": "10951a97c700476032ce239c7cd8d001",
+    "url": "/HW4-react/static/media/repost.10951a97.svg"
+  },
+  {
+    "revision": "3df4d79c46da0cdf4b8889e9b03564f7",
+    "url": "/HW4-react/static/media/share.3df4d79c.svg"
+  },
+  {
+    "revision": "2605ad4e59694e1b38336ddf66a85bbe",
+    "url": "/HW4-react/static/media/third.2605ad4e.jpg"
+  },
+  {
+    "revision": "4ee74cc78373593b14502ab7d6a63faf",
+    "url": "/HW4-react/static/media/tick.4ee74cc7.svg"
+  }
+]);
