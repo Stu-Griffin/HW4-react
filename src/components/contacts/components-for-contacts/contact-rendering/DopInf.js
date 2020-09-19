cfcf34a8ee8e731981a167054ebb057f5a46ecdb
Max <maksimavramenko33@gmail.com>
2020-09-19 20:20:57 +0300

    did part of advance

---
+++ src/components/contacts/components-for-contacts/contact-rendering/DopInf.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const DopInf = (props) => {
+    return(
+        <div>
+            <h1>{props.gender}</h1>
+            <h1>{props.phone}</h1>
+        </div>
+    )
+}
+export default DopInf;
--- src/components/contacts/components-for-contacts/contracts.js
+++ src/components/contacts/components-for-contacts/contracts.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import DopInf from './contact-rendering/DopInf.js';
+import {HashRouter, Route, Switch, Link} from "react-router-dom";
 import Contact from './contact-rendering/contact.js';
 import maleGender from '../../../assets/img/for-contacts/gender-man.svg';
 import femaleGender from '../../../assets/img/for-contacts/gender-woman.svg';
@@ -7,31 +9,38 @@ const contacts = [{
     firstName: "Барней",
     lastName: "Стинсовський",
     phone: "+380956319521",
-    gender: "male"
+    gender: "male",
+    link: "Барней-Стинсовський"
 }, {
     firstName: "Робін",
     lastName: "Щербатська",
     phone: "+380931460123",
-    gender: "female"
+    gender: "female",
+    link: "Робін-Щербатська"
 }, {
     firstName: "Анонімний",
     lastName: "Анонімус",
-    phone: "+380666666666"
+    gender: "anonim",
+    phone: "+380666666666",
+    link: "Анонімний-Анонімус"
 }, {
     firstName: "Лілія",
     lastName: "Олдровна",
     phone: "+380504691254",
-    gender: "female"
+    gender: "female",
+    link: "Лілія-Олдровна"
 }, {
     firstName: "Маршен",
     lastName: "Еріксонян",
     phone: "+380739432123",
-    gender: "male"
+    gender: "male",
+    link: "Маршен-Еріксонян"
 }, {
     firstName: "Теодор",
     lastName: "Мотсбес",
     phone: "+380956319521",
-    gender: "male"
+    gender: "male",
+    link: "Теодор-Мотсбес"
 }];
 class Contracts extends Component {
     state = {
@@ -63,22 +72,34 @@ class Contracts extends Component {
     }
     render() {
         return(
-            <section>
-                <input className="input" placeholder="search" value={this.state.inputValue} onChange={this.ChangeInputValue}/>
-                <div className="scrin">
-                    {this.state.data.map((el, i) => {
-                        return(
-                            <Contact
-                                key={i}
-                                firstName={el.firstName}
-                                gender={this.changeGender(el.gender)}
-                                phone={el.phone}
-                                lastName={el.lastName}
-                            /> 
-                        )     
-                    })}
-                </div>
-            </section>
+                <section>
+                    <input className="input" placeholder="search" value={this.state.inputValue} onChange={this.ChangeInputValue}/>
+                    <div className="scrin">
+                        {this.state.data.map((el, i) => {
+                            return(
+                                <HashRouter key={i}>
+                                    <Link to={`/contacts/${el.link}`}>
+                                        <Contact
+                                            key={i}
+                                            firstName={el.firstName}
+                                            gender={this.changeGender(el.gender)}
+                                            phone={el.phone}
+                                            lastName={el.lastName} // way={el.link}
+                                        /> 
+                                    </Link>
+                                    <Switch>
+                                        <Route path={`/contacts/${el.link}`}>
+                                            <DopInf
+                                                gender = {el.gender}
+                                                phone = {el.phone}
+                                            />
+                                        </Route>
+                                    </Switch>
+                                </HashRouter>
+                            )    
+                        })}
+                    </div>
+                </section>
         )
     }
 }
